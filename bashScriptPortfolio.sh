53e133eeca1e628a7834a5f794a0a6acc9f2cf01
halee-t <halee.tisler@365smartshop.com>
2023-06-28 11:25:27 -0400

    Finished coding. Fixed the if statements and added all ridles and portfolio pieces.

--- bashScriptPortfolio.sh
+++ bashScriptPortfolio.sh
@@ -1,45 +1,101 @@
-#! /bin/bash
+#!/bin/bash
 
-# declaring variables
+# declaring contact info variables
 name='Halee Tisler'
 occupation='Software Developer'
 email='haleetisler@gmail.com'
 linkedIn='linkedin.com/in/halee-tisler/'
 gitHub='github.com/halee-t'
+
+# declaring portfolio variables
+experience='I am a student at Oakland University studying to complete a Bachelors Degree in
+Computer Science. I am currently fulfilling an internship at 365 Retail Markets as a
+Software Developer Intern.'
+skills='Languages: Java - Git - HTML - CSS - C# - C - Bash
+Skills: GitFlow - Unit Testing - Agile
+Tools: GitHub - IntelliJ - Postman - Jira'
+hobbies='Outside of coding, my hobbies include fitness, board games, caring for my house plants,
+and embroidery'
+
+# play response variables
 yes='y'
 no='n'
 
+# borders for my digital business card
 borderTop="┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓"
 borderBottom="┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛"
 
-#riddles array
-declare -a riddleQ=("The more you take, the more you leave behind. What am I?"
-                      "I am an odd number. Take away a letter and I become even. What number am I?"
-                      "Poor people have it. Rich people need it. If you eat it you die. What is it?"
-                      "What is always on its way but never arrives?"
+# variables for the arrays
+index=0
+length=2
+
+#riddles array Q is questions, A is answers
+declare -a riddleQ=("The more you take, the more you leave behind. What am I? "
+                      "I am an odd number. Take away a letter and I become even. What number am I? "
+                      "What is always on its way but never arrives? "
 )
+declare -a riddleA=("Footsteps" "Seven" "Tomorrow")
 
-declare -a riddleA=("Footsteps" "Seven" "Nothing" "Tomorrow")
-
-#welcome
-echo "Hello! You have stumbled along $name's portfolio!"
+#welcome message
+echo -e "\nHello! You have stumbled along $name's portfolio!\n"
 echo "Here you will learn about $name's experience, skills, and hobbies."
-echo "However, you must answer riddles along the way ..."
+echo -e "However, you must answer riddles along the way ...\n"
 read -p "Would you like to play? y/n: " play
 
-if [[$play = $yes]]
+# player said yes to playing
+if [[ "$play" = "$yes" ]]
 then
-  #stuff
-  echo "Thanks for playing! Let's stay in touch :)"
-elif [[$play = $no]]
+  #riddle 1 unlocks experience
+  echo ""
+  read -p "Riddle 1: ${riddleQ[index]}" ans1
+  while [[ $ans1 != ${riddleA[index]} ]] ; do
+    read -p "Incorrect. (Hint: Answers must start with an uppercase letter) Try again: " ans1
+  done
+
+  #share experience and increment the index
+  echo -e "\nCongratulations! You have unlocked EXPERIENCE:\n$experience"
+  (( index += 1 ))
+  if [[ $index -gt $length ]]
+  then
+    (( index = 0 ))
+  fi
+
+  #riddle 2 unlocks skills
+  echo ""
+  read -p "Riddle 2: ${riddleQ[index]}" ans2
+  while [[ $ans2 != ${riddleA[index]} ]] ; do
+    read -p "Incorrect. (Hint: Answers must start with an uppercase letter) Try again: " ans2
+  done
+
+  #share skills and increment the index
+  echo -e "\nCongratulations! You have unlocked SKILLS:\n$skills"
+  (( index += 1 ))
+  if [[ $index -gt $length ]]
+  then
+    (( index = 0 ))
+  fi
+
+  #riddle 3 unlocks hobbies
+  echo ""
+  read -p "Riddle 3: ${riddleQ[index]}" ans3
+  while [[ $ans3 != ${riddleA[index]} ]] ; do
+      read -p "Incorrect. (Hint: Answers must start with an uppercase letter) Try again: " ans3
+  done
+
+  #share hobbies and exit
+  echo -e "\nCongratulations! You have unlocked HOBBIES:\n$hobbies"
+
+  echo -e "\nThanks for playing! Let's stay in touch :)"
+
+#player said no to playing
+elif [[ $play == $no ]]
 then
-  echo "Come back soon! Let's stay in touch :)"
+  echo -e "\nCome back soon! Let's stay in touch :)"
+
+# player entered and invalid response
 else
-  echo "Invalid Response"
+  echo -e "\nInvalid Response"
 fi
-
-
-#riddle 1 unlocks experience
 
 # printing contact info
 echo "$borderTop"
@@ -50,7 +106,3 @@ echo "  $email"
 echo "  $gitHub"
 echo "  $linkedIn"
 echo "$borderBottom"
-
-
-
-
